52f7487efecf73c100e568f732b3d0810b89a65a
Fede C <federico.cipolletta@diestroweb.com>
2023-09-03 00:50:24 -0300

    Muestro lista de palabras y letras

--- js/main.js
+++ js/main.js
@@ -8,14 +8,18 @@ const inputWordButton = document.getElementById("customPromptBtn"),
     mailInput = document.querySelector("#emailInput"),
     passwordInput = document.querySelector("#passwordInput"),
     //nameInput = document.querySelector("#nameInput"),
-    loginMsgLabel = document.querySelector("#loginMsgLabel");
+    loginMsgLabel = document.querySelector("#loginMsgLabel"),
+    userNameLabel = document.querySelector("#userNameLabel"),
+    scoresText = document.querySelector("#scoresText"),
+    ulWords = document.querySelector("#ulWords");
 
 const vocales = ["a", "e", "i", "o", "u"];
 const USER_NOT_FOUND = 0, WRONG_PASSWORD = 1, LOGIN_OK = 2;
 class User {
-    constructor(password, wordList) {
+    constructor(password, wordList, maxScore) {
         this.password = password;
         this.wordList = wordList;
+        this.maxScore = maxScore;
     }
 }
 
@@ -73,55 +77,63 @@ currentWordList.push(new InputWord("Mesa", "M"));
 currentWordList.push(new InputWord("Milanesa", "N"));
 currentWordList.push(new InputWord("Acondicionado", "N"));*/
 
-registerButton.addEventListener('click', function (event) {
-    loginMsgLabel.textContent = '';
 
-    if (customForm.checkValidity()) {
-        const user = JSON.parse(localStorage.getItem(mailInput.value));
-        if (user != null) {
+//esto pasa en indx.html
+if (registerButton)
+    registerButton.addEventListener('click', function (event) {
+        loginMsgLabel.textContent = '';
 
-            loginMsgLabel.textContent = 'El usuario ya existe!';
-            loginMsgLabel.className = '';
-            loginMsgLabel.classList.add('redLabel');
+        if (customForm.checkValidity()) {
+            const user = JSON.parse(localStorage.getItem(mailInput.value));
+            if (user != null) {
+
+                loginMsgLabel.textContent = 'El usuario ya existe!';
+                loginMsgLabel.className = '';
+                loginMsgLabel.classList.add('redLabel');
+            }
+            else {
+                localStorage.setItem(mailInput.value, JSON.stringify(new User(passwordInput.value, [], 0)));
+                mailInput.vale = "";
+                passwordInput.value = "";
+                loginMsgLabel.textContent = 'Registrado!';
+                loginMsgLabel.className = '';
+                loginMsgLabel.classList.add('greenLabel');
+            }
         }
         else {
-            localStorage.setItem(mailInput.value, JSON.stringify(new User(passwordInput.value, [])));
-            mailInput.vale = "";
-            passwordInput.value = "";
-            loginMsgLabel.textContent = 'Registrado!';
+            loginMsgLabel.textContent = 'Complete ambos campos';
             loginMsgLabel.className = '';
-            loginMsgLabel.classList.add('greenLabel');
+            loginMsgLabel.classList.add('redLabel');
         }
-    }
-    else {
-        loginMsgLabel.textContent = 'Complete ambos campos';
-        loginMsgLabel.className = '';
-        loginMsgLabel.classList.add('redLabel');
-    }
-    inputWordButton.style.display = 'none';
-    startCountButton.style.display = 'none';
-});
+        inputWordButton.style.display = 'none';
+        startCountButton.style.display = 'none';
+    });
 
-customForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    switch (checkPasswordAndLoadList(mailInput.value, passwordInput.value)) {
-        case USER_NOT_FOUND:
-            console.log("Usuario No Encontrado");
-            loginMsgLabel.textContent = 'Usuario no encontrado!';
-            loginMsgLabel.className = '';
-            loginMsgLabel.classList.add('redLabel');
-            break;
-        case WRONG_PASSWORD:
-            console.log("Contraseña Incorrecta");
-            loginMsgLabel.textContent = 'Contraseña incorrecta';
-            loginMsgLabel.className = '';
-            loginMsgLabel.classList.add('redLabel');
-            break;
-        case LOGIN_OK:
-            console.log("Login ok usuario: " + mailInput.value + " -->> Voy a pagina del juego");
-            break;
-    }
-});
+//esto pasa en indx.html
+if (customForm)
+    customForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        switch (checkPasswordAndLoadList(mailInput.value, passwordInput.value)) {
+            case USER_NOT_FOUND:
+                console.log("Usuario No Encontrado");
+                loginMsgLabel.textContent = 'Usuario no encontrado!';
+                loginMsgLabel.className = '';
+                loginMsgLabel.classList.add('redLabel');
+                break;
+            case WRONG_PASSWORD:
+                console.log("Contraseña Incorrecta");
+                loginMsgLabel.textContent = 'Contraseña incorrecta';
+                loginMsgLabel.className = '';
+                loginMsgLabel.classList.add('redLabel');
+                break;
+            case LOGIN_OK:
+                console.log("Login ok usuario: " + mailInput.value + " -->> Voy a pagina del juego");
+                sessionStorage.setItem("loggedUser", mailInput.value)
+                window.location.href = '../paginas/game.html'
+                break;
+        }
+    });
+
 
 function checkPasswordAndLoadList(input_mail, input_password) {
     const user = JSON.parse(localStorage.getItem(input_mail));
@@ -130,148 +142,179 @@ function checkPasswordAndLoadList(input_mail, input_password) {
     else if (user.password != input_password)
         return WRONG_PASSWORD;
     else {
-        currentWordList = user.wordList;//Cargo la lista
         return LOGIN_OK;
     }
 }
 
+let user;
+//esto pasa en game.html
+if (userNameLabel) {
+    const loggedUser = sessionStorage.getItem("loggedUser");
+    if (loggedUser) {
+        userNameLabel.textContent = "Usuario: " + loggedUser;
+        //console.log("Usuario: " + loggedUser);
+        user = JSON.parse(localStorage.getItem(loggedUser));
+        console.log(user);
+        scoresText.textContent = "Puntaje maximo: " + user.maxScore;
+        currentWordList = user.wordList;
+        console.log(currentWordList);
+        crearLista(currentWordList);
+    }
+    else {
+        console.log("Got here without user! How dare you? Send you back to login page ;)");
+        window.location.href = '../index.html'
+    }
+}
 
+
+function crearLista(arr) {
+    let wordElement;
+    if (arr.length)
+        for (const el of arr) {
+            wordElement = `
+      <li class=listElement> ${el.word} Letra: ${el.letra} <li>
+      `;
+            //se la agrego al contenedor
+            // ulWords.appendChild(wordElement);.
+            ulWords.innerHTML = ulWords.innerHTML + wordElement;
+        }
+}
 
 //Manejo del prompt
-inputWordButton.addEventListener("click", async () => {
-    let inputCorrect = false;
-    let tries = 3;
-    do {
+if (inputWordButton)
+    inputWordButton.addEventListener("click", async () => {
+        let inputCorrect = false;
+        let tries = 3;
+        do {
 
-        const { value: promptValues } = await Swal.fire({
-            title: 'Ingrese una palabra, y la letra a contar',
-            html:
-                '<input id="palabra" class="swal2-input" placeholder="Palabra">' +
-                '<input id="letra" class="swal2-input" placeholder="Letra a contar">',
-            focusConfirm: false,
-            preConfirm: () => {
-                return [
-                    document.getElementById('palabra').value,
-                    document.getElementById('letra').value
-                ];
+            const { value: promptValues } = await Swal.fire({
+                title: 'Ingrese una palabra, y la letra a contar',
+                html:
+                    '<input id="palabra" class="swal2-input" placeholder="Palabra">' +
+                    '<input id="letra" class="swal2-input" placeholder="Letra a contar">',
+                focusConfirm: false,
+                preConfirm: () => {
+                    return [
+                        document.getElementById('palabra').value,
+                        document.getElementById('letra').value
+                    ];
+                }
+            });
+
+            if (promptValues[0].length == 0 || promptValues[1].length == 0) {
+                alert("Por favor complete los 2 campos");
+                inputCorrect = false;
+                continue;
             }
-        });
 
-        if (promptValues[0].length == 0 || promptValues[1].length == 0) {
-            alert("Por favor complete los 2 campos");
-            inputCorrect = false;
-            continue;
+            console.log("Palabra: ", promptValues[0]);
+            console.log("Letra: ", promptValues[1]);
+
+            if (promptValues[1].length != 1) {
+                alert(`Se ingresó mas de un carater en el campo "Letra". Ingresar una letra únicamente`);
+                inputCorrect = false;
+                continue;
+            }
+
+            if (promptValues[1].toLowerCase() === promptValues[1].toUpperCase()) {
+                alert(`Se ingresó "${promptValues[1]}" en el campo "Letra".\nSolo se admiten letras.`);
+                inputCorrect = false;
+                continue;
+            }
+            if (hasNumbers(promptValues[0])) {
+                alert(`Se ingresó "${promptValues[0]}" en el campo "Palabra".\nSolo se admiten letras (sin numeros).`);
+                inputCorrect = false;
+                continue;
+            }
+            if (promptValues[0].length < 3) {
+                alert(`Se ingresó " ${promptValues[1]}" en el campo "Palabra".\nPor favor ingrese una palabra de 3 letras o mas`);
+                inputCorrect = false;
+                continue;
+            }
+            inputCorrect = true;
+
+            currentWordList.push(new InputWord(promptValues[0], promptValues[1]));
+            if (currentWordList.length >= 5) {
+                startCountButton.style.display = 'block';
+                orderWordsButton.style.display = 'block'
+            }
+            console.log("--> Lista de palabras: ");
+            console.log(currentWordList);
+
+
+        } while (!inputCorrect && --tries !== 0);
+    });
+
+if (startCountButton)
+    startCountButton.addEventListener("click", async () => {
+        borrarPuntaje();
+        for (const [index, element] of currentWordList.entries()) {
+            console.log("Analizando elemento: " + element.word);
+            let userAnswer = prompt("Cuantas veces aparecía la letra solicitada en la " + (index + 1) + "° palabra ingresada?\nAprete Cancelar salir");
+
+            if (userAnswer === null || userAnswer == "FIN")
+                break;
+
+            console.log("El usuario ingresó: " + userAnswer + " , y la respuesta correcta es: " + element.contarLetra());
+
+            let numAnswer = parseInt(userAnswer);
+            element.computarPuntaje(numAnswer);
+            console.log("Consiguio: " + element.puntajeObtenido_letras + " de " + element.puntaje)
         }
 
-        console.log("Palabra: ", promptValues[0]);
-        console.log("Letra: ", promptValues[1]);
-
-        if (promptValues[1].length != 1) {
-            alert(`Se ingresó mas de un carater en el campo "Letra". Ingresar una letra únicamente`);
-            inputCorrect = false;
-            continue;
-        }
-
-        if (promptValues[1].toLowerCase() === promptValues[1].toUpperCase()) {
-            alert(`Se ingresó "${promptValues[1]}" en el campo "Letra".\nSolo se admiten letras.`);
-            inputCorrect = false;
-            continue;
-        }
-        if (hasNumbers(promptValues[0])) {
-            alert(`Se ingresó "${promptValues[0]}" en el campo "Palabra".\nSolo se admiten letras (sin numeros).`);
-            inputCorrect = false;
-            continue;
-        }
-        if (promptValues[0].length < 3) {
-            alert(`Se ingresó " ${promptValues[1]}" en el campo "Palabra".\nPor favor ingrese una palabra de 3 letras o mas`);
-            inputCorrect = false;
-            continue;
-        }
-
-        inputCorrect = true;
-
-
-
-        currentWordList.push(new InputWord(promptValues[0], promptValues[1]));
-        if (currentWordList.length >= 5) {
-            startCountButton.style.display = 'block';
-            orderWordsButton.style.display = 'block'
-        }
-        console.log("--> Lista de palabras: ");
+        console.log("--->Resultados:")
         console.log(currentWordList);
 
+        //Sumo el puntaje total y lo muestro
+        calcularYmostrarResultado('Cuenta de Letras', "puntajeObtenido_letras");
+    });
 
-    } while (!inputCorrect && --tries !== 0);
-});
+if (orderWordsButton)
+    orderWordsButton.addEventListener("click", async () => {
+        borrarPuntaje();
+        for (const [index, element] of currentWordList.entries()) {
+            console.log("Analizando elemento: " + element.word);
+            let userAnswer = prompt("Ingrese la " + (index + 1) + "° palabra ingresada previamente\nAprete Cancelar para salir");
 
-startCountButton.addEventListener("click", async () => {
-    borrarPuntaje();
-    for (const [index, element] of currentWordList.entries()) {
-        console.log("Analizando elemento: " + element.word);
-        let userAnswer = prompt("Cuantas veces aparecía la letra solicitada en la " + (index + 1) + "° palabra ingresada?\nAprete Cancelar salir");
-
-        if (userAnswer === null || userAnswer == "FIN")
-            break;
-
-        console.log("El usuario ingresó: " + userAnswer + " , y la respuesta correcta es: " + element.contarLetra());
-
-        let numAnswer = parseInt(userAnswer);
-        element.computarPuntaje(numAnswer);
-        console.log("Consiguio: " + element.puntajeObtenido_letras + " de " + element.puntaje)
-    }
-
-    console.log("--->Resultados:")
-    console.log(currentWordList);
-
-    //Sumo el puntaje total y lo muestro
-    calcularYmostrarResultado('Cuenta de Letras', "puntajeObtenido_letras");
-});
-
-orderWordsButton.addEventListener("click", async () => {
-    borrarPuntaje();
-    for (const [index, element] of currentWordList.entries()) {
-        console.log("Analizando elemento: " + element.word);
-        let userAnswer = prompt("Ingrese la " + (index + 1) + "° palabra ingresada previamente\nAprete Cancelar para salir");
-
-        if (userAnswer === null || userAnswer == "FIN")
-            break;
+            if (userAnswer === null || userAnswer == "FIN")
+                break;
 
 
-        const foundElement = currentWordList.find(el => el.word.toUpperCase() === userAnswer.toUpperCase());
+            const foundElement = currentWordList.find(el => el.word.toUpperCase() === userAnswer.toUpperCase());
 
-        if (foundElement) {
-            console.log("El usuario ingresó: " + userAnswer + " , y la respuesta correcta en la posicion " + index + " es: " + element.word);
-            let indexOfFoundElement = currentWordList.indexOf(foundElement);
-            console.log("La palabra igresada se encuentra en la posicion: " + indexOfFoundElement);
-            const distancia = indexOfFoundElement - index;
-            switch (distancia) {
-                case 0:        //Si esta en la posicion correcta suma su puntaje
-                    element.puntajeObtenido_palabras += element.puntaje;
-                    break;
-                case 1:            //Si esta a 1 orden de distancia suma la mitad (+1 o -1)
-                case -1:
-                    element.puntajeObtenido_palabras += element.puntaje / 2;
-                    break
-                default:        //si esta a mas de un orde de distancia no suma nada
+            if (foundElement) {
+                console.log("El usuario ingresó: " + userAnswer + " , y la respuesta correcta en la posicion " + index + " es: " + element.word);
+                let indexOfFoundElement = currentWordList.indexOf(foundElement);
+                console.log("La palabra igresada se encuentra en la posicion: " + indexOfFoundElement);
+                const distancia = indexOfFoundElement - index;
+                switch (distancia) {
+                    case 0:        //Si esta en la posicion correcta suma su puntaje
+                        element.puntajeObtenido_palabras += element.puntaje;
+                        break;
+                    case 1:            //Si esta a 1 orden de distancia suma la mitad (+1 o -1)
+                    case -1:
+                        element.puntajeObtenido_palabras += element.puntaje / 2;
+                        break
+                    default:        //si esta a mas de un orde de distancia no suma nada
 
-                    break;
+                        break;
+                }
+
+                console.log("Suma en este palabra: " + element.puntajeObtenido_palabras);
+
+            } else {
+                console.log("No se encuentra el elemento, resta el puntaje de la palabra correcta");
+                element.puntajeObtenido_palabras -= element.puntaje;
+                continue; // sigo con la proxima palabra
             }
-
-            console.log("Suma en este palabra: " + element.puntajeObtenido_palabras);
-
-        } else {
-            console.log("No se encuentra el elemento, resta el puntaje de la palabra correcta");
-            element.puntajeObtenido_palabras -= element.puntaje;
-            continue; // sigo con la proxima palabra
         }
-    }
 
-    console.log("--->Resultados:")
-    console.log(currentWordList);
+        console.log("--->Resultados:")
+        console.log(currentWordList);
 
-    //Sumo el puntaje total y lo muestro
-    calcularYmostrarResultado('Orden de Palabras', "puntajeObtenido_palabras");
-})
+        //Sumo el puntaje total y lo muestro
+        calcularYmostrarResultado('Orden de Palabras', "puntajeObtenido_palabras");
+    })
 
 
 function hasNumbers(inputString) {
